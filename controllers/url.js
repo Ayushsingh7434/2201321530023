5d621ab256a901bebc9fd309582584fff473311f
Ayush Singh Bhumihar <ayushbhu1304@gmail.com>
2025-07-03 13:08:40 +0530

    abc

---
+++ controllers/url.js
@@ -0,0 +1,25 @@
+const { shortId } = require('shortid');
+const URL = require('../models/url');
+const shortid = require('shortid');
+async function handlegenerateNewShortURL(req, res) {
+  const body = req.body;
+  if(!body.url) {
+    return res.status(400).json({
+      error: 'URL is required'
+    });
+  }
+  const shortId = shortid();
+  await URL.create({
+    shortId: shortId,
+    redirectURL: body.url,
+    visitHistory: [],
+    });
+    
+    return res.json({
+      id: shortId
+    });
+}
+module.exports = {
+  handlegenerateNewShortURL,
+
+};
